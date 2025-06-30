f0cf870af013402428d2bf94bbe97d476a3ab8ec
Nj0be <beniamino@vagnarelli.dev>
2025-06-30 16:40:48 +0200

    Localization

--- lib/l10n/app_localizations_it.dart
+++ lib/l10n/app_localizations_it.dart
@@ -9,16 +9,98 @@ class AppLocalizationsIt extends AppLocalizations {
   AppLocalizationsIt([String locale = 'it']) : super(locale);
 
   @override
-  String get viewYourProfileTitle => 'Il tuo Profilo';
+  String get appName => 'Reverie';
 
   @override
-  String get viewProfileTitle => 'Profilo';
+  String get allDiaries => 'Tutti i Diari';
 
   @override
-  String get editProfileTitle => 'Modifica Profilo';
+  String get diary => 'Diario';
+
+  @override
+  String get timeCapsule => 'Capsule del Tempo';
+
+  @override
+  String get navigationDrawer => 'Menu';
+
+  @override
+  String get account => 'Account';
+
+  @override
+  String get email => 'Email';
 
   @override
   String get username => 'Username';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get confirmPassword => 'Conferma Password';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get signup => 'Signup';
+
+  @override
+  String get title => 'Titolo';
+
+  @override
+  String get page => 'Pagina';
+
+  @override
+  String get subPage => 'Sotto Pagina';
+
+  @override
+  String get day => 'Giorno';
+
+  @override
+  String get delete => 'Elimina';
+
+  @override
+  String get cancel => 'Cancella';
+
+  @override
+  String get moveUp => 'Sposta sopra';
+
+  @override
+  String get moveDown => 'Sposta sotto';
+
+  @override
+  String get newDiary => 'Nuovo Diario';
+
+  @override
+  String get create => 'Crea';
+
+  @override
+  String get save => 'Salva';
+
+  @override
+  String get createDiaryMessage => 'Stai creando il tuo diario!';
+
+  @override
+  String get editDiaryPageMessage => 'Stai modificando il tuo diario!';
+
+  @override
+  String get createDiary => 'Crea diario';
+
+  @override
+  String get editDiaryMessage => 'Stai modificando il tuo diario!';
+
+  @override
+  String get createTimeCapsuleMessage =>
+      'Stai creando la tua capsula del tempo!';
+
+  @override
+  String get content => 'Contenuto';
+
+  @override
+  String get yourProfile => 'Il tuo profilo';
 
   @override
   String get name => 'Nome';
@@ -27,29 +109,205 @@ class AppLocalizationsIt extends AppLocalizations {
   String get surname => 'Cognome';
 
   @override
-  String get editProfileButton => 'Modifica';
+  String get editProfile => 'Modifica profilo';
 
   @override
-  String get errorMessage => 'Errore';
+  String get emailMandatory => 'L\'email è obbligatoria';
 
   @override
-  String get logoutButton => 'Logout';
+  String get usernameMandatory => 'Lo username è obbligatorio';
 
   @override
-  String get saveButton => 'Salva';
+  String get nameMandatory => 'Il nome è obbligatorio';
 
   @override
-  String get settings => 'Impostazioni';
+  String get surnameMandatory => 'Il cognome è obbligatorio';
 
   @override
-  String get help => 'Aiuto e Feedbacks';
+  String get titleMandatory => 'Il titolo è obbligatorio';
 
   @override
-  String get diariesNavigationButton => 'Diari';
+  String get contentMandatory => 'Il contenuto è obbligatorio';
 
   @override
-  String get profileNavigationButton => 'Profilo';
+  String get passwordMandatory => 'La password è obbligatoria';
 
   @override
-  String get userNotAuthenticated => 'Utente non autenticato';
+  String get passwordsDontMatch => 'Le due password non corrispondono';
+
+  @override
+  String get passwordLength => 'La password deve avere almeno 8 caratteri';
+
+  @override
+  String get usernameLength => 'Lo username deve avere almeno 3 caratteri';
+
+  @override
+  String get sendResetPasswordEmail => 'Invia email di reset password';
+
+  @override
+  String get goBackToLogin => 'Torna al login';
+
+  @override
+  String get resetPassword => 'Reset password';
+
+  @override
+  String get resetPasswordEmailSent => 'Email per reset password inviata';
+
+  @override
+  String get resetPasswordEmailError => 'Errore nell\'invio dell\'email';
+
+  @override
+  String get goToSignup => 'Non hai un account? Registrati';
+
+  @override
+  String get forgotPassword => 'Hai dimenticato la password?';
+
+  @override
+  String get alreadyHaveAccount => 'Hai già un account? Effettua il login';
+
+  @override
+  String get signupSuccessful =>
+      'Registrazione avvenuta con successo. Torna al login.';
+
+  @override
+  String get signupError => 'Errore di registrazione';
+
+  @override
+  String get loginError => 'Errore di login';
+
+  @override
+  String get noDateSelected => 'Nessuna data selezionata';
+
+  @override
+  String get date => 'Data';
+
+  @override
+  String get selectDate => 'Seleziona data';
+
+  @override
+  String get error => 'Errore';
+
+  @override
+  String get usernameAlreadyTaken => 'Username già utilizzato';
+
+  @override
+  String get emailAlreadyTaken => 'Email già utilizzata';
+
+  @override
+  String get selectADate => 'Seleziona una data';
+
+  @override
+  String get emailNotValid => 'Email non valida';
+
+  @override
+  String get saveChanges => 'Salva modifiche';
+
+  @override
+  String get updateError => 'Errore di aggiornamento';
+
+  @override
+  String get confirmDiaryDeletion => 'Conferma eliminazione diario';
+
+  @override
+  String get deleteDiary =>
+      'Sei sicuro di voler cancellare questo diario? Questa opzione non può essere annullata.';
+
+  @override
+  String get confirmPageDeletion => 'Conferma eliminazione pagina';
+
+  @override
+  String get deletePage =>
+      'Sei sicuro di voler cancellare questa pagina? Questa opzione non può essere annullata.';
+
+  @override
+  String get movePrevSubpage => 'Muovi alla precedente sotto pagina';
+
+  @override
+  String get moveNextSubpage => 'Muovi alla prossima sotto pagina';
+
+  @override
+  String get receiverMandatory => 'Almeno un destinatario è necessario';
+
+  @override
+  String get deadlineMandatory => 'La data di scadenza è necessaria';
+
+  @override
+  String get missingPhonePrefix =>
+      'Numero non valido, aggiungi il prefisso (es. +39)';
+
+  @override
+  String get phoneNumberNotValid => 'Numero non valido';
+
+  @override
+  String get phoneNumber => 'Numero di telefono';
+
+  @override
+  String get phoneNumberAlreadySelected => 'Numero di telefono già selezionato';
+
+  @override
+  String get emailAlreadySelected => 'Email già selezionata';
+
+  @override
+  String get editDiary => 'Modifica diario';
+
+  @override
+  String get selectImage => 'Seleziona immagine';
+
+  @override
+  String get arrivalDate => 'Data di arrivo';
+
+  @override
+  String get creationDate => 'Data di creazione';
+
+  @override
+  String get receiver => 'Destinatario';
+
+  @override
+  String get receivers => 'Destinatari';
+
+  @override
+  String get sender => 'Mittente';
+
+  @override
+  String get contentNotAvailable => 'Contenuto non disponibile';
+
+  @override
+  String get letterContent => 'Contenuto della lettera';
+
+  @override
+  String get profile => 'Profilo';
+
+  @override
+  String get pageNumber => 'Numero di pagine';
+
+  @override
+  String get createTimeCapsule => 'Crea capsula del tempo';
+
+  @override
+  String get letterForTheFuture => 'Lettera per il futuro';
+
+  @override
+  String get letterForTheFutureDescription =>
+      'Questa lettera serve per mandare un messaggio nel futuro';
+
+  @override
+  String get createdOn => 'Creata il';
+
+  @override
+  String get arrivingOn => 'In arrivo il';
+
+  @override
+  String get arrivedOn => 'Arrivata il';
+
+  @override
+  String get editDiaryPage => 'Modifica pagina di diario';
+
+  @override
+  String get add => 'Aggiungi';
+
+  @override
+  String get enterPhoneNumber => 'Inserisci un numero di telefono';
+
+  @override
+  String get enterEmail => 'Inserisci un\'email';
 }
