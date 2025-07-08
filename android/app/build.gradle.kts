3e887a445465cdfc65b7078e3202f149a4928c5a
Nj0be <beniamino@vagnarelli.dev>
2025-07-08 18:06:38 +0200

    Enabled flutter optimization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,14 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(
+                // Default file with automatically generated optimization rules.
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+            )
         }
     }
 }
